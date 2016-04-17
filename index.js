8ccde2dedd3fa8c276e544de950ffb35f1d83de0
Andres Duebi <andres.duebi@hpe.com>
2016-04-17 21:29:05 +0200

    fixes #3 in a different way:
    - d13eebcb broke the milestone creation for me
    - var `err` is in fact undefined in index.js#L70, therefore passing `null`

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ if (settings.gitlab.projectID === null) {
       console.log(milestoneDataMapped);
       console.log('\n\n\n\n\n\n\n');
       
-      async.each(data.title, function(item, cb) {
+      async.each(data, function(item, cb) {
         if (milestoneDataMapped.indexOf(item.title) < 0) {
           console.log('Creating new Milestone', item.title);
           createMilestone(item, function(err, createMilestoneData) {
@@ -67,7 +67,7 @@ if (settings.gitlab.projectID === null) {
             return cb(err);
           });
         } else {
-          return cb(err);
+          return cb(null);
         }
       }, function(err) {
         if (err) return console.log(err);
