9bc607e745dca132a8805892d3cc41261dcd828d
Lorin Halpert <me@lorinhalpert.com>
2022-10-15 22:18:12 -0400

    Added ext unicode stripping and trim
    Ok, this works nicely now!

--- src/index.ts
+++ src/index.ts
@@ -331,7 +331,9 @@ async function transferMilestones(usePlaceholders: boolean) {
  */
 async function transferLabels(attachmentLabel = true, useLowerCase = true) {
   inform('Transferring Labels');
-  console.warn(CCWARN,'NOTE (2022): GitHub descriptions are limited to 100 characters!');
+  console.warn(CCWARN,'NOTE (2022): GitHub descriptions are limited to 100 characters, and do not accept 4-byte Unicode');
+
+  const invalidUnicode = /[\u{10000}-\u{10FFFF}]/gu;
 
   // Get a list of all labels associated with this project
   let labels: SimpleLabel[] = await gitlabApi.Labels.all(
@@ -366,15 +368,24 @@ async function transferLabels(attachmentLabel = true, useLowerCase = true) {
     if (!githubLabels.find(l => l === label.name)) {
       console.log('Creating: ' + label.name);
 
-      if (label.description.length > 100) {
-        const trimmedDescription = label.description.slice(0,100);
-        if (settings.trimOversizedLabelDescriptions) {
-          console.warn(CCWARN,`Description too long (${label.description.length}), it was trimmed: \n "${label.description}"\n\t to\n "${trimmedDescription}"`);
-          label.description = trimmedDescription;
-        } else {
-          console.warn(CCWARN,`Description too long (${label.description.length}), it was excluded.`);
-          console.debug(` "${label.description}"`);
-          label.description = '';
+      if (label.description) {
+        if (label.description.match(invalidUnicode)) {
+          console.warn(CCWARN,`⚠️ Removed invalid unicode characters from description.`);
+          const cleanedDescription = label.description.replace(invalidUnicode, '').trim();
+          console.debug(` "${label.description}"\n\t to\n "${cleanedDescription}"`);
+          label.description = cleanedDescription;
+        }
+        if (label.description.length > 100) {
+          const trimmedDescription = label.description.slice(0,100).trim();
+          if (settings.trimOversizedLabelDescriptions) {
+            console.warn(CCWARN,`⚠️ Description too long (${label.description.length}), it was trimmed:`);
+            console.debug(` "${label.description}"\n\t to\n "${trimmedDescription}"`);
+            label.description = trimmedDescription;
+          } else {
+            console.warn(CCWARN,`⚠️ Description too long (${label.description.length}), it was excluded.`);
+            console.debug(` "${label.description}"`);
+            label.description = '';
+          }
         }
       }
 
